aecd05d4f9cb67adc07dd5a9d76e346c124736f0
Richard Baptist <code@richardbaptist.nl>
2026-07-03 21:39:37 +0200

    Add scaled stat calculation

--- spec/models/base_pokemon_spec.rb
+++ spec/models/base_pokemon_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe BasePokemon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#stat_at_level" do
+    let(:base_pokemon) { BasePokemon.new(hp: 35, attack: 55) }
+
+    it "computes hp with the level and +10 bonus" do
+      expect(base_pokemon.stat_at_level(:hp, 10)).to eq((2 * 35 * 10 / 100) + 10 + 10)
+    end
+
+    it "computes other stats with the +5 bonus" do
+      expect(base_pokemon.stat_at_level(:attack, 10)).to eq((2 * 55 * 10 / 100) + 5)
+    end
+  end
 end
